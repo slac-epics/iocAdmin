5b78f63931f6bee685c7f69b4428e023c4398094
Remy Machet <rmachet@slac.stanford.edu>
2009-03-31 01:47:55 +0000

    -Fixed labca build. -Fixed acqiris build. -Added a new variable in RELEASE_SITE: EXTERNAL_PACKAGES that points to a dircetory containing external dependencies. -Fixed realclean target in RULES.Display (I broke it).
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk@191 ececa60f-9f63-0410-97c7-ea05888eb260

--- extensions/src/labca/html/manual/images.pl
+++ extensions/src/labca/html/manual/images.pl
@@ -1,18 +1,42 @@
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2002-2-1 (1.70)
 # Associate images original text with physical files.
 
+
+$key = q/>;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$&gt;$">|; 
+
+$key = q/>=10;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="48" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$&gt;=10$">|; 
 
 $key = q/=10+INVALID_ALARM;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="205" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
+ SRC="|."$dir".q|img6.png"
  ALT="$=10 + INVALID\_ALARM$">|; 
+
+$key = q/<;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="15" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$&lt;$">|; 
 
 $key = q/=10+INVALID_ALARM+1;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="233" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img1.png"
+ SRC="|."$dir".q|img5.png"
  ALT="$= 10 + INVALID\_ALARM + 1$">|; 
+
+$key = q/<1;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="28" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$&lt;1$">|; 
 
 1;
 
