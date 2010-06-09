44fe4dd8b5da6e6f18602d416ff4bab1c58786f2
Bruce Hill <bhill@slac.stanford.edu>
2010-06-09 09:24:29 +0000

    Added new screen for IOC's with iBootBar power control: ioc_rtems_power Added new screen and launch script for developers: ioc_screen_demo.edl Made home screen name be a macro $(HOME)
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/modules/iocAdmin/current@3079 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ edm.cmd
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Setup edm environment
+PACKAGE_TOP=/reg/g/pcds/package
+export EPICS_SITE_TOP=$PACKAGE_TOP/epics/3.14
+export EPICS_HOST_ARCH=$($EPICS_SITE_TOP/base/current/startup/EpicsHostArch.pl)
+
+export EDMFILES=$EPICS_SITE_TOP/extensions/current/templates/edm
+export EDMFILTERS=$EPICS_SITE_TOP/extensions/current/templates/edm
+export EDMHELPFILES=$EPICS_SITE_TOP/extensions/current/src/edm/helpFiles
+export EDMLIBS=$EPICS_SITE_TOP/extensions/current/lib/$EPICS_HOST_ARCH
+export EDMOBJECTS=$EPICS_SITE_TOP/extensions/current/templates/edm
+export EDMPVOBJECTS=$EPICS_SITE_TOP/extensions/current/templates/edm
+export EDMUSERLIB=$EPICS_SITE_TOP/extensions/current/lib/$EPICS_HOST_ARCH
+export EDMACTIONS=$PACKAGE_TOP/tools/edm/config
+
+# For defining CALC PVs in EDM
+export EDMCALC=$EPICS_SITE_TOP/extensions/current/src/edm/setup/calc.list
+
+export EDMDATAFILES=.:..
+
+pathmunge ()
+{
+	if [ ! -d $1 ] ; then
+		return
+	fi
+	if ! echo $PATH | /bin/egrep -q "(^|:)$1($|:)" ; then
+		if [ "$2" = "after" ] ; then
+			PATH=$PATH:$1
+		else
+			PATH=$1:$PATH
+		fi
+	fi
+}
+
+pathmunge $EPICS_SITE_TOP/base/current/bin/$EPICS_HOST_ARCH after
+pathmunge $EPICS_SITE_TOP/extensions/current/bin/$EPICS_HOST_ARCH after
+unset pathmunge
+
+# Launch edm with iocAdmin demo screen
+edm -x -eolc -m "ioc=AMO:R14:IOC:41,HOME=AMOHome.edl,HOST=AMO-R14-IOC-41" iocAdminLib/srcDisplay/ioc_screen_demo.edl &
