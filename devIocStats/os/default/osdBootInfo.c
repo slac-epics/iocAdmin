bd66ce971c642fd6929739506462dc1c93a02f3e
Ernest Williams <ernesto@slac.stanford.edu>
2012-02-08 15:34:20 +0000

    merge in changes from devIocStats-3.1.8 to our iocAdmin-R3-1-6-RC1-lcls1

--- devIocStats/os/default/osdBootInfo.c
+++ devIocStats/os/default/osdBootInfo.c
@@ -9,7 +9,7 @@
 * in file LICENSE that is included with this distribution.
 \*************************************************************************/
 
-/* osdBootInfo.c - Boot info strings: default implementation = do nothing */
+/* osdBootInfo.c - Boot info strings: default implementation */
 
 /*
  *  Author: Ralph Lange (HZB/BESSY)
@@ -20,32 +20,18 @@
  *
  */
 
+#define SYSBOOTLINE_NEEDED
 #include <devIocStats.h>
-
-static char *notimpl = "<not implemented>";
 
 int devIocStatsInitBootInfo (void) { return 0; }
 
 int devIocStatsGetStartupScript (char **pval)
 {
-    *pval = notimpl;
-    return -1;
+    return devIocStatsGetStartupScriptDefault(pval);
 }
 
 int devIocStatsGetBootLine (char **pval)
 {
-    *pval = notimpl;
-    return -1;
-}
-
-int devIocStatsGetEngineer (char **pval)
-{
-    *pval = notimpl;
-    return -1;
-}
-
-int devIocStatsGetLocation (char **pval)
-{
-    *pval = notimpl;
-    return -1;
+    *pval = sysBootLine;
+    return 0;
 }
